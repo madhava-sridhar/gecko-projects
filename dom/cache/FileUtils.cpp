615896bef2e466689864510824b54e43091b8985
Ben Kelly <ben@wanderview.com>
2014-09-30 22:22:32 -0400

    Relax error if a body file cannot be deleted. This currently fails on windows mochitest.

--- dom/cache/FileUtils.cpp
+++ dom/cache/FileUtils.cpp
@@ -401,8 +401,11 @@ FileUtils::BodyDeleteFiles(nsIFile* aBaseDir, CacheId aCacheId,
       continue;
     }
 
+    // TODO: figure out why files cannot be removed on windows mochitest
     rv = finalFile->Remove(false /* recursive */);
-    if (NS_WARN_IF(NS_FAILED(rv))) { return rv; }
+    if (NS_FAILED(rv)) {
+      NS_WARNING("Failed to remove Cache body file!");
+    }
   }
 
   return rv;
